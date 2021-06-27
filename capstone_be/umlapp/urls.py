4c3e1b0955a1afe9fa9a3e77c2e82bcf298d30c7
connor-hood <connor_hood@yahoo.com>
2021-06-27 14:29:46 -0500

    added endpoint for blank url

--- capstone_be/umlapp/urls.py
+++ capstone_be/umlapp/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from . import views
 
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/songs')),
     path('songs/', views.SongList.as_view()),
     path('songs/<int:pk>/', views.SongDetail.as_view()),
     path('playlists/', views.PlaylistList.as_view()),
