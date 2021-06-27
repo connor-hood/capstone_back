b39a18dd9e2e7d5a53701c238130d19dce35c421
connor-hood <connor_hood@yahoo.com>
2021-06-27 17:37:33 -0500

    fk fields added and migrated with no issue

--- capstone_be/umlapp/admin.py
+++ capstone_be/umlapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Playlist, Song
+from .models import User, Playlist, Song, Favorite
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Playlist)
 admin.site.register(Song)
+admin.site.register(Favorite)
--- capstone_be/umlapp/models.py
+++ capstone_be/umlapp/models.py
@@ -4,12 +4,21 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     name = models.CharField(max_length=30)
+    playlist = models.ForeignKey('Playlist',
+                                 on_delete=models.CASCADE,
+                                 related_query_name='Playlist.user')
+    favorite = models.ForeignKey('Favorite', on_delete=models.CASCADE)
 
 
 class Playlist(models.Model):
     playlist_name = models.CharField(max_length=50)
     playlist_description = models.CharField(max_length=200)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_query_name='User.playlist')
+    song = models.ForeignKey('Song',
+                             on_delete=models.CASCADE,
+                             related_query_name='Song.Playlist')
 
 
 class Song(models.Model):
@@ -18,8 +27,12 @@ class Song(models.Model):
     album = models.CharField(max_length=100)
     ranking = models.IntegerField()
     artwork = models.URLField()
-    playlist = models.ManyToManyField(Playlist)
+    playlist = models.ManyToManyField(Playlist,
+                                      related_query_name='Playlist.song')
 
 
-class Favorites(models.Model):
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
+class Favorite(models.Model):
+    name = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_query_name='User.favorite')
+    song = models.ManyToManyField(Song)
