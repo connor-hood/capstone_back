e15151ffe935e2e80185fb0344d7044cbd0031f9
connor-hood <connor_hood@yahoo.com>
2021-06-27 11:14:25 -0500

    models migrated into db

--- capstone_be/umlapp/admin.py
+++ capstone_be/umlapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Playlist, Song
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Playlist)
+admin.site.register(Song)
--- capstone_be/umlapp/models.py
+++ capstone_be/umlapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Playlist(models.Model):
+    playlist_name = models.CharField(max_length=50)
+    playlist_description = models.CharField(max_length=200)
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=50)
+    artist = models.CharField(max_length=50)
+    album = models.CharField(max_length=100)
+    ranking = models.IntegerField()
+    artwork = models.URLField()
+
+
