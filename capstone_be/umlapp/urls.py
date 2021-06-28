a6a5478b706c27cb7d104e0230fd7cc7df90683f
connor-hood <connor_hood@yahoo.com>
2021-06-28 13:27:07 -0500

    added views for users, trying to make view and url for users playlists

--- capstone_be/umlapp/urls.py
+++ capstone_be/umlapp/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/songs')),
+    path('', RedirectView.as_view(url='/users')),
     path('songs/', views.SongList.as_view()),
     path('songs/<int:pk>/', views.SongDetail.as_view()),
     path('playlists/', views.PlaylistList.as_view()),
-
+    path('playlists/<int:pk>/', views.PlaylistDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('users/<int:pk>/playlists/', views.PlaylistDetail.as_view())
 ]
