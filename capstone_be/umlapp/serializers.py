85c5b4f9270b1287b01a3717c534f1a6b5e5716b
connor-hood <connor_hood@yahoo.com>
2021-06-28 13:49:46 -0500

    added favorite serializer and views, added fk fields to serialzier

--- capstone_be/umlapp/serializers.py
+++ capstone_be/umlapp/serializers.py
@@ -1,21 +1,26 @@
 from rest_framework import serializers
-from .models import User, Playlist, Song
+from .models import User, Playlist, Song, Favorite
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'playlist', 'favorite']
 
 
 class PlaylistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Playlist
-        fields = ['id', 'playlist_name', 'playlist_description']
+        fields = ['id', 'playlist_name', 'playlist_description', 'user', 'song']
 
 
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ['id', 'title', 'artist', 'album', 'ranking', 'artwork']
+        fields = ['id', 'title', 'artist', 'album', 'ranking', 'artwork', 'playlist']
 
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = ['id', 'name', 'song']
--- capstone_be/umlapp/views.py
+++ capstone_be/umlapp/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from .models import Song, Playlist, User
-from .serializers import SongSerializer, PlaylistSerializer, UserSerializer
+from .models import Song, Playlist, User, Favorite
+from .serializers import SongSerializer, PlaylistSerializer, UserSerializer, FavoriteSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
 
 # Create your views here.
+# song views
 class SongList(APIView):
 
     def get(self, request):
@@ -49,6 +50,7 @@ class SongDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+# playlist views
 class PlaylistList(APIView):
 
     def get(self, request):
@@ -91,6 +93,7 @@ class PlaylistDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+# User views
 class UserList(APIView):
 
     def get(self, request):
@@ -130,4 +133,46 @@ class UserDetail(APIView):
     def delete(self, request, pk):
         user = self.get_object(pk)
         user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# favorite views
+class FavoriteList(APIView):
+    def get(self, request):
+        favorite = Favorite.objects.all()
+        serializer = FavoriteSerializer(favorite, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = FavoriteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class FavoriteDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Favorite.objects.get(pk=pk)
+        except Favorite.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        favorite = self.get_object(pk)
+        serializer = FavoriteSerializer(favorite)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        favorite = self.get_object(pk)
+        serializer = FavoriteSerializer(favorite, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        favorite = self.get_object(pk)
+        favorite.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
