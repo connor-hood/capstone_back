0bbf8cea6412999dbd0030c35e72a2b499a79e6b
connor-hood <connor_hood@yahoo.com>
2021-06-28 16:44:58 -0500

    wrote user playlist detail, testing all endpoints in postman

--- capstone_be/umlapp/views.py
+++ capstone_be/umlapp/views.py
@@ -185,8 +185,35 @@ class UserPlaylistList(APIView):
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
+        serializer = PlaylistSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserPlaylistDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Playlist.objects.get(pk=pk)
+        except Playlist.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        playlist = self.get_object(pk)
+        serializer = PlaylistSerializer(playlist)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        playlist = self.get_object(pk)
+        serializer = PlaylistSerializer(playlist, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        playlist = self.get_object(pk)
+        playlist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
