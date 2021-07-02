90d07c03ece58dd7c244235af68585397eec4ec2
connor-hood <connor_hood@yahoo.com>
2021-07-02 17:25:19 -0500

    removed more jwt stuff to get data from server in postman

--- capstone_be/capstone_be/settings.py
+++ capstone_be/capstone_be/settings.py
@@ -127,17 +127,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-    ),
-}
-
-
-
-
